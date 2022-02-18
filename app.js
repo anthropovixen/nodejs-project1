9c0f72b74cb0ac92974a0999edfff806f050e539
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-02-18 16:16:57 +1100

    Update app.js
    Clear for next exercise

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-const fs = require("fs");
 
-// fs.writeFileSync("notes.txt", "My name is Tanimara!");
-fs.appendFileSync(
-  "notes.txt",
-  "I am a junior software developer and I love to learn Node JS!"
-);
