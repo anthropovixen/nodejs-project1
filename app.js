7f9a445d042175f094c8b1e3c7bbcb288a807b84
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-02-18 16:06:27 +1100

    Update app.js
    Appending message to file. Answer to challenge exercise.

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const fs = require("fs");
 
-fs.writeFileSync("notes.txt", "My name is Tanimara!");
+// fs.writeFileSync("notes.txt", "My name is Tanimara!");
+fs.appendFileSync(
+  "notes.txt",
+  "I am a junior software developer and I love to learn Node JS!"
+);
