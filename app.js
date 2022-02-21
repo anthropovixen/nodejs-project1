156c0dfdca58dfb8ee3bfd974c4d7393a7a1cd8b
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-02-21 12:15:44 +1100

    Update app.js
    Answer to challenge on using the chalk npm package
    Require chalk package
    Print on console with specific style using chalk

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const chalk = require("chalk");
+
 const validator = require("validator");
 
 const getNotes = require("./notes.js");
@@ -8,3 +10,5 @@ console.log(notes);
 
 console.log(validator.isEmail("tanimara@email.com"));
 console.log(validator.isURL("https://anthropovixen.github.io/tanimarasantos/"));
+
+console.log(chalk.green.bold.inverse("Success!"));
