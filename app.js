be0ee7db18a39ecea62581be7a7cb42d3d186f26
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-02-28 13:59:20 +1100

    Update app.js
    Require use of yargs package
    
    Use yargs command to add arguments to process object
    
    Cange console.log(yargs.argv) by yargs.parse() that make it accessible the same way.

--- app.js
+++ app.js
@@ -1,12 +1,56 @@
 const chalk = require("chalk");
+const yargs = require("yargs");
 const getNotes = require("./notes.js");
 
-const command = process.argv[2];
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "body of note",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title: " + argv.title);
+    console.log("Body: " + argv.body);
+  },
+});
 
-console.log(process.argv);
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing the note");
+  },
+});
 
-if (command === "add") {
-  console.log("Adding note!");
-} else if (command === "remove") {
-  console.log("Removing note!");
-}
+// Create list command
+yargs.command({
+  command: "list",
+  describe: "List the notes",
+  handler: function () {
+    console.log("Listing the notes");
+  },
+});
+
+// Create read command
+yargs.command({
+  command: "read",
+  describe: "Read the note",
+  handler: function () {
+    console.log("Reading the note");
+  },
+});
+
+// add, remove, read, list
+yargs.parse();
+//console.log(yargs.argv);
