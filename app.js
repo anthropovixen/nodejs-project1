b929050db6c6cbd19ccc69a7baacec7102ce8404
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-02-28 12:56:55 +1100

    Update app.js
    Remove code for next exercise
    Create variable to store the argument vector on the third position of the process object
    Conditional to show adding note to console if there is an add at the variable, or to add removing note if there is a remove to variable

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const chalk = require("chalk");
-
-const validator = require("validator");
-
 const getNotes = require("./notes.js");
 
-const notes = getNotes();
-
-console.log(notes);
-
-console.log(validator.isEmail("tanimara@email.com"));
-console.log(validator.isURL("https://anthropovixen.github.io/tanimarasantos/"));
-
-console.log(chalk.blue.bold.inverse("Success!"));
+const command = process.argv[2];
 
 console.log(process.argv);
+
+if (command === "add") {
+  console.log("Adding note!");
+} else if (command === "remove") {
+  console.log("Removing note!");
+}
