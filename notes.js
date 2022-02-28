6511f6dde09d4afc050583af57e12adcf3ef4336
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-02-28 16:27:44 +1100

    Update notes.js
    Require file system to use it to write and read files
    
    Create removeNote function by:
    - loading notes
    -filtering notes by title
    -popping notes from file if the title match
    - saving new notes with note removed
    
    Create function addNote by:
    - loading notes
    -filtering notes by title and adding it to variable duplicateNotes
    - if the note is not duplicated, then change notes adding the new one
    - saving new notes with note added
    
    Create saveNotes function by:
    - creating dataJSON variable to stringify notes
    - write on file the dataJSON
    
    Create loadNotes function by:
    - try to see if there is a file notes.json that can be read, data converted to strings and then parsed into JSON format.If not, then return empty array
    
    Exports commands to be used

--- notes.js
+++ notes.js
@@ -1,5 +1,62 @@
+const fs = require("fs");
+
 const getNotes = function () {
   return "Your notes...";
 };
+const removeNote = function (title, body) {
+  const notes = loadNotes();
+  const removeNote = notes.filter(function (note) {
+    return note.title === title;
+  });
+  if (removeNote.length !== 0) {
+    notes.pop({
+      title: title,
+    });
+    saveNotes(notes);
+    console.log("Note deleted");
+  } else {
+    console.log("Title non existent");
+  }
+};
+// Load in the notes
+const addNote = function (title, body) {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(function (note) {
+    return note.title === title;
+  });
 
-module.exports = getNotes;
+  if (duplicateNotes.length === 0) {
+    // Change notes
+    notes.push({
+      title: title,
+      body: body,
+    });
+
+    // Save notes
+    saveNotes(notes);
+    console.log("New note added!");
+  } else {
+    console.log("Note title taken!");
+  }
+};
+
+const saveNotes = function (notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+const loadNotes = function () {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+  removeNote: removeNote,
+};
