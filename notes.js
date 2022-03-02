082b9f5ec3079ec325ad7207f815949c52b07e8b
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-03-02 13:17:47 +1100

    Update notes.js
    Add comments

--- notes.js
+++ notes.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 const chalk = require("chalk");
 
-const getNotes = () => {
-  return "Your notes...";
-};
-
-// Load in the notes
 const addNote = (title, body) => {
+  // Load in the notes
   const notes = loadNotes();
+  // Search through the notes until a match is found
   const duplicateNote = notes.find((note) => note.title === title);
+  // if there is not match, then the new note will be added
+
+  debugger;
 
   if (!duplicateNote) {
     // Change notes
@@ -20,47 +20,71 @@ const addNote = (title, body) => {
     // Save notes
     saveNotes(notes);
     console.log(chalk.green.inverse("New note added!"));
+    // if there is note with the same title, then the error message will be shown
   } else {
     console.log(chalk.red.inverse("Note title taken!"));
   }
 };
 
+// remove note method
 const removeNote = (title) => {
+  // load in the notes
   const notes = loadNotes();
+  // filter through every message to remove note from notes
   const removeNote = notes.filter((note) => note.title !== title);
-
+  // if notes is bigger than removeNote, then the note has been removed
   if (notes.length > removeNote.length) {
     console.log(chalk.green.inverse("Note removed"));
+    // new notes has been saved
     saveNotes(removeNote);
+    // if notes is not bigger than removeNote, then the note has not been found
   } else {
     console.log(chalk.red.inverse("No note found"));
   }
 };
+
+// List notes
 const listNotes = () => {
+  // load in notes
   const notes = loadNotes();
+  // Message to print on console
   console.log(chalk.blue.inverse("Your notes"));
+  // Loop through each note and write on console their titles
   notes.forEach((note) => {
     console.log(note.title);
   });
 };
 
+// Read notes
 const readNote = (title) => {
+  // Load in notes
   const notes = loadNotes();
+  // Find notes that having matching titles
   const readNote = notes.find((note) => note.title === title);
+  // if there is a matching title
   if (readNote) {
+    // then, print title and body on console
     console.log(chalk.blue.inverse(readNote.title));
     console.log(readNote.body);
+    // if there is not a matching title, then note is not found
   } else {
     console.log(chalk.red.inverse("Note not found!"));
   }
 };
 
+// Save notes
 const saveNotes = (notes) => {
+  // stringify data to transform into JSON format
   const dataJSON = JSON.stringify(notes);
+  // write new data on file notes.json
   fs.writeFileSync("notes.json", dataJSON);
 };
 
+// loadNotes
 const loadNotes = () => {
+  // try to read data from notes.json
+  // converting it into string
+  // parse it into object JSON
   try {
     const dataBuffer = fs.readFileSync("notes.json");
     const dataJSON = dataBuffer.toString();
@@ -70,8 +94,8 @@ const loadNotes = () => {
   }
 };
 
+// Export methods
 module.exports = {
-  getNotes: getNotes,
   addNote: addNote,
   removeNote: removeNote,
   listNotes: listNotes,
